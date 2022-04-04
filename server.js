61a593e188a289f748f2d58c08a371603ecfd018
MariusWik <Petre.marius@gmail.com>
2022-04-04 16:11:51 +0300

    postgr

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const db = knex({
   client: "pg",
   connection: {
     host: "process.env.DATABASE_URL", //localhost
-    ssl:true
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
 });
 
