f6cab9eaacc55e781cd869865d98dccfc59a3aff
MariusWik <Petre.marius@gmail.com>
2022-04-04 16:14:24 +0300

    postgr

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require("./controllers/image.js");
 const db = knex({
   client: "pg",
   connection: {
-    host: "process.env.DATABASE_URL", //localhost
+    connectionString: process.env.DATABASE_URL, //localhost
     ssl: {
       rejectUnauthorized: false
     }
