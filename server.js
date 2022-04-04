96cd50cd72f1b0dde43d56978956ecab54e00480
MariusWik <Petre.marius@gmail.com>
2022-04-04 13:41:30 +0300

    add new

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require("./controllers/register.js");
+const signin = require("./controllers/signin.js");
+const profile = require("./controllers/profile.js");
+const image = require("./controllers/image.js");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1", //localhost
+    user: "postgres",
+    password: "test",
+    database: "smart-brain",
+  },
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/signin", signin.handleSignin(db, bcrypt));
+
+app.get("/profile/:id", profile.handleProfileGet(db));
+app.put("/image/", image.handleImage(db));
+app.post("/imageurl/", image.handleApiCall());
+
+app.post("/register", register.handleRegister(db, bcrypt));
+
+app.listen(3000, () => {
+  console.log("app is runnign on port 3000");
+});
